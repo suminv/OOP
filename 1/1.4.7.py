6865566ab7a361bb75d6d902cc5f1e67bed4457c
Vlad <vlads1976@gmail.com>
2022-06-09 16:29:31 +0200

    Add solved task 1.4.7

---
+++ 1/1.4.7.py
@@ -0,0 +1,33 @@
+class CPU:
+    def __init__(self, name, fr):
+        self.name = name
+        self.fr = fr
+
+
+class Memory:
+    def __init__(self, name, volume):
+        self.name = name
+        self.volume = volume
+
+
+class MotherBoard:
+    def __init__(self, name, cpu, mem_slots, total_mem_slots=4):
+
+        self.name = name
+        self.cpu = cpu
+        self.total_mem_slots = total_mem_slots
+        self.mem_slots = mem_slots
+
+    def get_config(self):
+        return [
+            f"Материнская плата: {self.name}",
+            f"Центральный процессор: {self.cpu.name}, {self.cpu.fr}",
+            f"Слотов памяти: {self.total_mem_slots}",
+            f"Память: {'; '.join([f'{i.name} - {i.volume}' for i in self.mem_slots])}"
+        ]
+
+
+cpu = CPU('intel', 1000)
+mem1, mem2 = Memory('Noname', 3600), Memory('Intel', 1000)
+mb = MotherBoard('MSI', cpu, [mem1, mem2])
+mb.get_config()
