0079cbc729c4764ef3735e8529445520f3783109
Vlad <vlads1976@gmail.com>
2022-06-10 15:18:49 +0200

    Add solved task 1.4.8 and 1.6.6

---
+++ 1/1.4.8.py
@@ -0,0 +1,45 @@
+class Cart:
+    def __init__(self):
+        self.goods = []
+
+    def add(self, gd):
+        self.goods.append(gd)
+
+    def remove(self, indx):
+        self.goods.pop(indx)
+
+    def get_list(self):
+        return [f'{i.name}: {i.price}' for i in self.goods]
+
+
+class Table:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class TV:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class Notebook:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class Cup:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+cart = Cart()
+cart.add(TV('LG', 25000))
+cart.add(TV('Sony', 100000))
+cart.add(Table('higashi', 10000))
+cart.add(Notebook('valeo', 200000))
+cart.add(Notebook('lenovo', 50000))
+cart.add(Cup('fq', 100))
---
+++ 1/1.6.6.py
@@ -0,0 +1,7 @@
+class AbstractClass:
+
+    def __new__(cls, *args, **kwargs):
+        return 'Ошибка: нельзя создавать объекты абстрактного класса'
+
+
+obj = AbstractClass()
