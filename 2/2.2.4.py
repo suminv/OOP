164d2357ebf77e0ea96e63df6cd998a19e804f34
Vlad <vlads1976@gmail.com>
2022-07-13 15:58:42 +0200

    Add solved tasks  2.2.4.py

---
+++ 2/2.2.4.py
@@ -0,0 +1,26 @@
+class Car:
+    def __init__(self, model=None):
+        self.__model = model
+
+
+    @property
+    def model(self):
+        return self.__model
+
+    @model.setter
+    def model(self, model):
+        if isinstance(model, str) and 2 < len(model) < 100:
+            self.__model = model
+
+
+
+car = Car()
+car.model = 'BMW'
+assert car.model == 'BMW'
+
+
+
+
+
+
+
